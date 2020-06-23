7a9972f07dad7eb6e7aacf650024e85920279cc1
Aaron Storrer <aaron@storrer.net>
2020-06-23 13:41:43 -0600

    Update expert.rb

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -2,10 +2,12 @@ class Expert < ApplicationRecord
   #has_shortened_urls
   has_many :tags, dependent: :destroy
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, :source => :expert
 
   after_destroy do |f|
-    Friendships.where(expert_id => f.expert_id).destroy unless nil?
+    if Friendship.where(:expert_id => f.friend_id, :friend_id => f.expert_id).present?
+      Friendship.destroy(:expert_id => f.friend_id, :friend_id => f.expert_id)
+    end
   end
 end
